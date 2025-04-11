3a4e086024fb8e0d17b69e6fad06c1f84d857001
Oleksandrdudkamagaz <liphonee023@gmail.com>
2025-04-11 08:30:11 +0300

    Update get_balance.py

--- get_balance.py
+++ get_balance.py
@@ -1,10 +1,10 @@
 import requests
 
 # Замість 'ваш_API_ключ' вставте ваш реальний API ключ
-REM_ONLINE_API_KEY = 'ваш_API_ключ'
+REM_ONLINE_API_KEY = 'ffdcb6e1038f410f9c56e234925e5940'
 
 # Вкажіть ID клієнта, баланс якого ви хочете отримати
-client_id = 'ID_клієнта'
+client_id = '7ca045ee-b85d-427b-92e7-a9aff6f9d13b'
 
 # Формуємо URL для запиту
 url = f'https://api.remonline.com/v1/clients/{client_id}/balance'
